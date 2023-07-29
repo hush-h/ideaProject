fa855531c432ddc4bc21a49edd237e2226f21caa
hush-sh <2457931884@qq.com>
2023-07-29 17:01:06 +0800

    我是master新增

--- heima/text1/src/com/heima/demo1/Demo1.java
+++ heima/text1/src/com/heima/demo1/Demo1.java
@@ -6,5 +6,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("helloWorld");
         System.out.println("helloWorld");
+        System.out.println("我是master新增");
     }
 }
