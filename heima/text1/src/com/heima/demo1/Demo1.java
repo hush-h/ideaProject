11e95367c95f927faaba2fdc22ad0d9800751844
hush-sh <2457931884@qq.com>
2023-07-29 16:26:05 +0800

    第二次提交代码

--- heima/text1/src/com/heima/demo1/Demo1.java
+++ heima/text1/src/com/heima/demo1/Demo1.java
@@ -5,5 +5,6 @@ import jdk.nashorn.internal.parser.JSONParser;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("helloWorld");
+        System.out.println("helloWorld");
     }
 }
